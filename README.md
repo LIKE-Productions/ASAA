899594ce11942a1aa99b43caef2f63b66da3ff3e
yappermags <magnuspetersonmunoz@gmail.com>
2021-01-12 16:46:25 -0700

    Prepare for Version 0.1A

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # ASAA
 
-ASAA is a simple app that aims to create a shot accruacy program using a Microsoft Kinect and an optional projector. We are not sure what language or what interface we are using yet. I am experimenting with different options to see which one works best. 
+## Description
+ASAA is a simple app that aims to create a shot accruacy program using a Microsoft Kinect and an optional projector.
+
+## Language
+We are hoping to be able to use Python as our main language mainly because it is widely compatible with many devices. I am currently running the code on a Raspberry Pi.
+
+# Development process
+This is the place where I explain the development process.
+
+## Versions:
+
+**January 9, 2021**: *Release 1.0 Alpha One*
+
+>For full information on the changes, view the [Changelog](/Changelog)
--- main.py
+++ main.py
@@ -2,13 +2,38 @@ from collections import Counter
 import math
 from playsound import playsound
 
+# Sound Variables
 bad = playsound('Sounds/bad.wav')
 okay = playsound('Sounds/okay.wav')
 good = playsound('Sounds/good.wav')
-
-left_right = [(26, 22), (26, 22), (28, 20), (30, 24), (28, 24), (28, 24)]
-# Counted with 60 frames
+# Hand variables
 left = [26, 26, 28, 30, 28, 28]
 right = [22, 22, 20, 24, 24, 24]
 
-lcomp = [good if l <= 24 else okay if l >=30 else bad for l in left]
+
+left_right = int(input("Are you left handed or right handed? 0 for left, 1 for right."))
+
+while left_right >=2:
+    left_right
+
+if left_right:
+    lcomp = [good if l <= 24 else okay if l >=28 else bad for l in left]
+else:
+    rcomp = [good if r <= 22 else okay if r >=24 else bad for r in right]
+# Juicy sweet code
+# Counted with 60 frames
+
+
+
+
+
+ileft = iter(left)
+iright = iter(right)
+
+print("Left hand")
+for i in ileft:
+    print(i)
+print("")
+print("Right hand")
+for i in iright:
+    print(i)
