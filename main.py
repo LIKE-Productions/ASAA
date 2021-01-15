2f548d358aa94053a5d52ddeceea296d251a4e66
yappermags <magnuspetersonmunoz@gmail.com>
2021-01-14 21:45:25 -0700

    Version 0.1A1, except for CHANGELOG

--- main.py
+++ main.py
@@ -1,39 +1,66 @@
 from collections import Counter
 import math
-from playsound import playsound
+from typing import Collection
+from pydub import AudioSegment
+import spow
+import pymongo
+
+client = pymongo.MongoClient(
+    "mongodb+srv://Magnus-Peters-Munzo:vB0$12!G!WAi@asaafun.zjiyv.mongodb.net/ASAAfun?retryWrites=true&w=majority")
+db = client.asaa
+asaa = db.asaa
+
 
 # Sound Variables
-bad = playsound('Sounds/bad.wav')
-okay = playsound('Sounds/okay.wav')
-good = playsound('Sounds/good.wav')
+good = AudioSegment.from_wav("/home/magnus/GitHub/ASAA/Sounds/good.wav")
+okay = AudioSegment.from_wav("/home/magnus/GitHub/ASAA/Sounds/okay.wav")
+bad = AudioSegment.from_wav("/home/magnus/GitHub/ASAA/Sounds/bad.wav")
+# DB Variables
+aleft = []
+aright = []
+# input variables
+left_right = 2
+
 # Hand variables
-left = [26, 26, 28, 30, 28, 28]
-right = [22, 22, 20, 24, 24, 24]
+# left = [26, 26, 28, 30, 28, 28]
+# right = [22, 22, 20, 24, 24, 24]
+dbleft = {}
+dbright = {}
 
 
-left_right = int(input("Are you left handed or right handed? 0 for left, 1 for right."))
+left_right = int(input("Are you left handed or right handed? 0 for left, 1 for right(-1 to end): "))
 
-while left_right >=2:
-    left_right
+if left_right == 0:
+    left_right = 0
+    print("Times should be posted in 60 fps. To convert 30 fps to 60 fps, take your number and multiply it by two.")
+    while True:
+        left = int(input(
+            "Name one throwing time you have. When you have entered in all your times, type in 0: "))
+        aleft.append(left)
 
-if left_right:
-    lcomp = [good if l <= 24 else okay if l >=28 else bad for l in left]
+        if left == 0:
+            False
+            dbleft = {
+                "handiness": "left",
+                "numbers": aleft
+            }
+            break
+elif left_right == 1:
+    left_right = 1
+    while True:
+        print("Times should be posted in 60 fps. To convert 30 fps to 60 fps, take your number and multiply it by two.")
+        right = float(input(
+            "Name one throwing time you have. When you have entered in all your times, type in 0: "))
+        aright.append(right)
+        if right == 0:
+            False
+            dbright = {
+                "handiness": "right",
+                "numbers": aright
+            }
+        break
 else:
-    rcomp = [good if r <= 22 else okay if r >=24 else bad for r in right]
-# Juicy sweet code
-# Counted with 60 frames
+    pass
 
-
-
-
-
-ileft = iter(left)
-iright = iter(right)
-
-print("Left hand")
-for i in ileft:
-    print(i)
-print("")
-print("Right hand")
-for i in iright:
-    print(i)
+left_id = asaa.insert_one(dbleft).inserted_id
+right_id = asaa.insert_one(dbright).inserted_id
